430724ee2c3572120f79a67c31df83c5517700f8
Libo Zhang <zhanglb21@mails.tsinghua.edu.cn>
2025-10-12 06:32:41 +0000

    update

--- eval.sh
+++ eval.sh
@@ -11,4 +11,4 @@ python eval_trans_res_memo_cross_vimo.py \
     --time_steps 10 \
     --ext rvq1_rtrans1_bs64_cs4_ts10 \
     --which_epoch all \
-    --test_txt test_spatial_explosion.txt
+    --test_txt test_spatial.txt
