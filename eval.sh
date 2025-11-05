9e14e5a35f633623b6d2569cbc98ca8492895c3e
Libo Zhang <zhanglb21@mails.tsinghua.edu.cn>
2025-11-05 11:44:02 +0000

    update 1105

--- eval.sh
+++ eval.sh
@@ -4,12 +4,12 @@ python eval_trans_res_memo_cross_vimo.py \
     --dataset_name vimo \
     --vq_name rvq_official \
     --name ar_dinoCond_traj_depth_1031 \
-    --exp_name ar_dinoCond_traj_depth_basketball \
+    --exp_name ar_dinoCond_traj_depth_cat \
     --res_name rtrans_official \
     --gpu_id 0 \
     --cond_scale 4 \
     --time_steps 10 \
     --ext rvq1_rtrans1_bs64_cs4_ts10 \
     --which_epoch all \
-    --test_txt test_spatial_basketball.txt \
+    --test_txt test_spatial_cat.txt \
     --use_depth 
