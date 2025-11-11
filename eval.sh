fc2ef594647615eb5fa2a6f682a7251c5e798a26
Libo Zhang <zhanglb21@mails.tsinghua.edu.cn>
2025-11-11 16:32:47 +0000

    update 1111

--- eval.sh
+++ eval.sh
@@ -3,13 +3,13 @@ export CUDA_VISIBLE_DEVICES=2
 python eval_trans_res_memo_cross_vimo.py \
     --dataset_name vimo \
     --vq_name rvq_official \
-    --name ar_wofix \
-    --exp_name ar_wofix \
+    --name ar_traj_depth_1108 \
+    --exp_name ours_football \
     --res_name rtrans_official \
     --gpu_id 0 \
     --cond_scale 4 \
     --time_steps 10 \
     --ext rvq1_rtrans1_bs64_cs4_ts10 \
     --which_epoch all \
-    --test_txt test_spatial.txt \
+    --test_txt football.txt \
     # --use_depth 
